3055f73f11a41b2d326c12debedbee66ed5ac90b
Nasif-Khan <nasif.sajid.khan@gmail.com>
2025-05-20 15:15:37 +0530

    disable tests

--- src/test/java/com/nasif/jounalApp/service/UserServiceTests.java
+++ src/test/java/com/nasif/jounalApp/service/UserServiceTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Disabled
 public class UserServiceTests {
     @Autowired
     private UserRepository userRepository;
